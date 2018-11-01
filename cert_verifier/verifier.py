5f9428732a5c662a7e5b3fe4d62975f5ea49dfd6
peara <quang.vn@outlook.com>
2018-11-01 21:39:33 +0700

    change return format

--- cert_verifier/verifier.py
+++ cert_verifier/verifier.py
@@ -45,16 +45,23 @@ def verify_certificate_file(certificate_file_name, transaction_id=None, options=
         certificate_model = to_certificate_model(certificate_json=certificate_json,
                                                        txid=transaction_id,
                                                        certificate_bytes=certificate_bytes)
-        result = verify_certificate(certificate_model, options)
+
+        # print(certificate_model.certificate_json)
+
+        result = {
+          'recipient': certificate_model.certificate_json['recipientProfile'],
+          'transaction_id': certificate_model.txid,
+          'issued_on': certificate_model.certificate_json['issuedOn'],
+          'result': verify_certificate(certificate_model, options)
+        }
     return result
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         for cert_file in sys.argv[1:]:
-            print(cert_file)
             result = verify_certificate_file(cert_file)
-            print(result)
+            exit(result)
     else:
         result = verify_certificate_file('../tests/data/2.0/valid.json')
-        print(result)
+        print("RESULT", result)
